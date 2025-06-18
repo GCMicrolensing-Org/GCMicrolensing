ccfd0de026235e1d485cb61da9ee349a9849b938
Amber <ambermalpas@gmail.com>
2025-06-18 11:44:04 -0400

    some restructuring

---
+++ GCMicrolensing/__init__.py
@@ -0,0 +1,17 @@
+# GCMicrolensing/__init__.py
+
+__version__ = '0.1.0'  # Start with a simple version number, update as you go!
+
+"""
+GCMicrolensing: A Python package by Greg Costa Cuautle for simulating astrometric data.
+
+This package provides tools for generating simulated astrometric data for
+microlensing events, including specialized functions for calculations,
+and utilities for creating engaging plots and animations.
+"""
+
+# You can also add import statements here if you want certain
+# functions/classes to be directly accessible when someone does
+# "from GCMicrolensing import ..."
+# For example:
+# from .models import SomeClass, another_function
